1e8267b3f05797217c28154ea48fa1ab265bd396
BylnovNick450504 <bylnov.nick@gmail.com>
2017-03-03 01:48:36 +0600

    add cross origin annotaion

--- src/main/java/com/owuteam/Application.java
+++ src/main/java/com/owuteam/Application.java
@@ -1,9 +1,13 @@
 package com.owuteam;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);

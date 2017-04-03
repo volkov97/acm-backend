51c09a13f50457247a9e42256272c44dba2f6712
BylnovNick450504 <bylnov.nick@gmail.com>
2017-04-04 02:49:27 +0600

    change on create-drop, delete test data

--- src/main/java/com/owuteam/core/DatabaseLoader.java
+++ src/main/java/com/owuteam/core/DatabaseLoader.java
@@ -35,48 +35,48 @@ public class DatabaseLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        User user1 = new User("Nick", "Bylnov", "Sergey",
-                              "bigboss", "12345", new String[]{"ROLE_USER"});
-        News newsItem1 = new News(
-               "Джава",
-                "Java",
-                "8",
-                "джава коре",
-                "java se",
-                "лямбда выражения",
-                "lambda",
-                8,
-                1,
-                1
-        );
-        News newsItem2 = new News(
-                "Пайтон",
-                "python",
-                "8",
-                "джава коре",
-                "java se",
-                "лямбда выражения",
-                "lambda",
-                10,
-                1,
-                1
-        );
-        Tag tag1 = new Tag("популярное", "popular");
-        Tag tag2 = new Tag("супе","super");
-        Topic topic1 = new Topic("бгуир","bsuir");
-        Topic topic2 = new Topic("ксис","ksis");
-
-
-        newsItem1.addTag(tag1);
-        newsItem1.setTopic(topic1);
-        newsItem1.addTag(tag2);
-        newsItem2.setTopic(topic2);
-        user1.addNews(newsItem1);
-        user1.addNews(newsItem2);
-        userRepository.save(user1);
-        newsRepository.save(newsItem1);
-        tagRepository.save(tag1);
-        topicRepository.save(topic1);
-        newsRepository.save(newsItem2);
+//        User user1 = new User("Nick", "Bylnov", "Sergey",
+//                              "bigboss", "12345", new String[]{"ROLE_USER"});
+//        News newsItem1 = new News(
+//               "Джава",
+//                "Java",
+//                "8",
+//                "джава коре",
+//                "java se",
+//                "лямбда выражения",
+//                "lambda",
+//                8,
+//                1,
+//                1
+//        );
+//        News newsItem2 = new News(
+//                "Пайтон",
+//                "python",
+//                "8",
+//                "джава коре",
+//                "java se",
+//                "лямбда выражения",
+//                "lambda",
+//                10,
+//                1,
+//                1
+//        );
+//        Tag tag1 = new Tag("популярное", "popular");
+//        Tag tag2 = new Tag("супе","super");
+//        Topic topic1 = new Topic("бгуир","bsuir");
+//        Topic topic2 = new Topic("ксис","ksis");
+//
+//
+//        newsItem1.addTag(tag1);
+//        newsItem1.setTopic(topic1);
+//        newsItem1.addTag(tag2);
+//        newsItem2.setTopic(topic2);
+//        user1.addNews(newsItem1);
+//        user1.addNews(newsItem2);
+//        userRepository.save(user1);
+//        newsRepository.save(newsItem1);
+//        tagRepository.save(tag1);
+//        topicRepository.save(topic1);
+//        newsRepository.save(newsItem2);
     }
 }

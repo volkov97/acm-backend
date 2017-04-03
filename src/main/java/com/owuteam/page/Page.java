283cb1f6561a59c1ff7d6ca16a3c7abf5fd80f2e
BylnovNick450504 <bylnov.nick@gmail.com>
2017-04-04 04:21:02 +0600

    add titleRU, titleEN, contentRU, contentEN, systemName to Page

--- src/main/java/com/owuteam/page/Page.java
+++ src/main/java/com/owuteam/page/Page.java
@@ -10,35 +10,67 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "acm_pages")
 public class Page extends BaseEntity {
-    private String title;
+    private String titleRU;
+    private String titleEN;
+    private String systemName;
 
     @Lob
     @Type(type = "text")
-    private String content;
+    private String contentRU;
+
+    @Lob
+    @Type(type = "text")
+    private String contentEN;
 
     public Page() {
         super();
     }
 
-    public Page(String title, String content) {
-        super();
-        this.title = title;
-        this.content = content;
+    public Page(String titleRU, String titleEN, String systemName, String contentRU, String contentEN) {
+        this.titleRU = titleRU;
+        this.titleEN = titleEN;
+        this.systemName = systemName;
+        this.contentRU = contentRU;
+        this.contentEN = contentEN;
     }
 
-    public String getTitle() {
-        return title;
+    public String getTitleRU() {
+        return titleRU;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setTitleRU(String titleRU) {
+        this.titleRU = titleRU;
     }
 
-    public String getContent() {
-        return content;
+    public String getTitleEN() {
+        return titleEN;
     }
 
-    public void setContent(String content) {
-        this.content = content;
+    public void setTitleEN(String titleEN) {
+        this.titleEN = titleEN;
+    }
+
+    public String getSystemName() {
+        return systemName;
+    }
+
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
+
+    public String getContentRU() {
+        return contentRU;
+    }
+
+    public void setContentRU(String contentRU) {
+        this.contentRU = contentRU;
+    }
+
+    public String getContentEN() {
+        return contentEN;
+    }
+
+    public void setContentEN(String contentEN) {
+        this.contentEN = contentEN;
     }
 }
